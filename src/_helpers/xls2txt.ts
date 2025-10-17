f98f080b2379d2cbf50214f7fc33532ae021fc95
jarielht <jherrera@wicon.io>
2025-10-16 22:09:07 -0500

    Actualizacion de funcion de autenticacion

---
+++ src/_helpers/xls2txt.ts
@@ -0,0 +1,59 @@
+import * as XLSX from "xlsx";
+import * as fs from "fs";
+import * as path from "path";
+import * as iconv from "iconv-lite";
+
+function exportExcelToTxt(inputPath: string, outputPath: string, delimiter: string) {
+    try {
+
+        if (!fs.existsSync(inputPath)) {
+            throw "El archivo especificado no existe.";
+        }
+
+        const ext = path.extname(inputPath).toLowerCase();
+        if (ext !== ".xls" && ext !== ".xlsx") {
+            throw "Formato no soportado. Solo se permiten archivos .xls o .xlsx.";
+        }
+
+        const workbook = XLSX.readFile(inputPath, {
+            raw: false,
+            type: "binary"
+        });
+
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        if (!sheet) {
+            throw "No se encontró ninguna hoja en el archivo Excel.";
+        }
+
+        const jsonData: any[] = XLSX.utils.sheet_to_json(sheet, {defval: ""});
+
+        if (jsonData.length === 0) {
+            throw "La hoja de Excel está vacía.";
+        }
+
+        const headers = Object.keys(jsonData[0]);
+        const rows: string[] = [];
+
+        rows.push(headers.join(delimiter));
+
+        for (const row of jsonData) {
+            const line = headers.map((h) => `${row[h] ?? ""}`).join(delimiter);
+            rows.push(line);
+        }
+
+        const txtContent = rows.join("\r\n");
+        const buffer = iconv.encode(txtContent, "utf16-le");
+
+        fs.writeFileSync(outputPath, buffer);
+
+        console.log(`✅ Archivo exportado correctamente a: ${outputPath}`);
+    }
+    catch (error: any) {
+        console.error("❌ Error al exportar el archivo:", error.message);
+    }
+}
+
+
+exportExcelToTxt("/Users/arielherrera/WebstormProjects/unibank-validator-server/archive/data/236/20251014/test.xlsx", "/Users/arielherrera/WebstormProjects/unibank-validator-server/archive/data/236/20251014/test.txt", "~");
